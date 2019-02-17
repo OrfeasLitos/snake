db5a58da25117c7ed2c44bec923c7f441f4221ab
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2019-02-17 14:25:30 +0000

    Animate snake

--- main.js
+++ main.js
@@ -3,5 +3,21 @@
 //drawGame(game)
 //document.onkeydown = onkeydown
 
+const SPEED = 0.001
+
+async function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
+async function play() {
+  draw(world)
+  world.step()
+
+  await delay(1/SPEED)
+  requestAnimationFrame(play)
+}
+
 const world = new World()
-draw(world)
+play()
