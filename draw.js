db9e2b020e2598ee1d56af243bca8b1f12ca39be
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2019-02-17 14:24:46 +0000

    Draw each block separately

--- draw.js
+++ draw.js
@@ -4,17 +4,20 @@ function render(square) {
   const x = (square.x / X_BLOCKS) * W
   const y = (square.y / Y_BLOCKS) * H
 
+  ctx.beginPath()
   ctx.moveTo(x, y)
   ctx.lineTo(x, y + BLOCK_DIST)
   ctx.lineTo(x + BLOCK_DIST, y + BLOCK_DIST)
   ctx.lineTo(x + BLOCK_DIST, y)
   ctx.closePath()
+  ctx.fill()
 }
 
 function draw(world) {
+  // TODO: optimize by drawing entire snake
+  // in one go and filling once
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   for (square of world.squares) {
     render(square)
   }
-  ctx.fill()
 }
