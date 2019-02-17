b2292b775ca93f3a51fd0ab2552ac8b0b5e45d4d
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2019-02-17 21:54:14 +0000

    Implement steering

--- main.js
+++ main.js
@@ -8,11 +8,24 @@ async function delay(ms) {
 
 async function play() {
   draw(world)
-  world.step()
-
   await delay(1/SPEED)
+  world.step()
   requestAnimationFrame(play)
 }
 
 const world = new World()
+
+document.onkeydown = (e) => {
+  e = e || window.event
+  const key = e.keyCode - 37
+
+  if ([0, 1, 2, 3].includes(key)) {
+    const dir = new Vector(
+      (key - 1) % 2, // -1,  0, 1, 0
+      (key - 2) % 2  //  0, -1, 0, 1
+    )
+    world.dir = dir
+  }
+}
+
 play()
--- snake.js
+++ snake.js
@@ -8,6 +8,8 @@ class World {
   constructor() {
     this.squares = []
     this.direction = new Vector(0, 1)
+    this.movesQueue = []
+
     this.head = new Vector(
       Math.floor(X_BLOCKS/2),
       Math.floor(Y_BLOCKS/2)
@@ -21,14 +23,21 @@ class World {
   }
 
   step() {
-    this.head = this.head.move(this.dir)
+    if (this.movesQueue.length > 0) {
+      this.direction = this.movesQueue.shift()
+    }
+    this.head = this.head.move(this.direction)
     this.squares.push(this.head.clone())
     this.squares.shift()
-  }
-}
 
   }
 
+  set dir(dir) {
+    const prev = (this.movesQueue.length === 0) ?
+      this.direction : this.movesQueue[0]
 
+    if (!prev.add(dir).isZero()) {
+      this.movesQueue.push(dir)
+    }
   }
 }
