dd1080423a16a772b8c4cb64aab3525f56bc4c7d
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2019-02-17 14:24:18 +0000

    Get correct canvas dimensions

--- size.js
+++ size.js
@@ -1,3 +1,8 @@
+const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
-const W = canvas.width
-const H = canvas.height
+const W = canvas.width = window.innerWidth
+const H = canvas.height = window.innerHeight
+
+window.onresize = () => {
+  //TODO: do not resize canvas
+}
